e6903cccb03b8ce55bb399962541352273af1b8b
Felicio Angga <felicioangga004@gmail.com>
2023-11-12 15:55:03 +0700

    add legend in pie chart

--- src/components/dashboard/Chart.jsx
+++ src/components/dashboard/Chart.jsx
@@ -43,8 +43,18 @@ function Chart() {
     <div className="p-5 rounded-xl shadow-md bg-white">
       <p className="text-xl font-semibold leading-[30px]">Pelaporan</p>
       <p className="text-sm text-[#777980] leading-5">Pelaporan Pelanggaran Sampah</p>
-
       <Pie className='mt-10 max-h-[154px]' options={options} data={data} />
+
+      <div className="mt-10 flex justify-between">
+        <div className="flex gap-[6px]">
+          <div className="my-auto w-[14px] h-[14px] rounded-full bg-[#4339F2]" />
+          <p className="text-sm">Individu</p>
+        </div>
+        <div className="flex gap-[6px]">
+          <div className="my-auto w-[14px] h-[14px] rounded-full bg-[#FF3A29]" />
+          <p className="text-sm">Perusahaan</p>
+        </div>
+      </div>
     </div>
   )
 }
