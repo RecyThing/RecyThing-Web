cbc68eb80ad4c34553835677e4fa80c41a174cc4
Felicio Angga <felicioangga004@gmail.com>
2023-11-13 23:41:10 +0700

    fix selected color log

--- src/components/navigation/SideBarItem.jsx
+++ src/components/navigation/SideBarItem.jsx
@@ -1,5 +1,7 @@
 import { PiCaretDown, PiCaretUp } from "react-icons/pi";
 import { useNavigate, useLocation } from "react-router-dom"
+import recycleWhite from '@/assets/recycle-white.png'
+import recycle from '@/assets/recycle.png'
 import PropTypes from 'prop-types';
 
 export function SideBarItem({ name, logo, path, subMenu, sideBarCollapse, setCollapse, expandedMenu, setExpandedMenu }) {
@@ -16,11 +18,12 @@ export function SideBarItem({ name, logo, path, subMenu, sideBarCollapse, setCol
   }
 
   const isSubMenuSelected = subMenu?.filter(e => e.path === pathname).length > 0;
-
+  
   if (sideBarCollapse) return (
     <div onClick={handleClickMenu} className={`cursor-pointer p-3 rounded-lg ${(pathname === path || isSubMenuSelected) ?
      'bg-[#35CC33] text-white' : 'hover:bg-slate-100'}`}>
-      {logo}
+      {name === "Transaksi Drop Point" ? <img src={(pathname === path || isSubMenuSelected) ? recycleWhite : recycle} 
+      alt="" className="shrink-0 w-6 h-6" /> : logo}
     </div>
   )
 
@@ -29,13 +32,14 @@ export function SideBarItem({ name, logo, path, subMenu, sideBarCollapse, setCol
       <div onClick={handleClickMenu} className={`pl-4 pr-3 py-3 flex justify-between rounded-lg
       ${(pathname === path || isSubMenuSelected) ? 'bg-[#35CC33] text-white' : 'hover:bg-slate-100'}`}>
         <div className='flex gap-4'>
-          {logo}
+          {name === "Transaksi Drop Point" ? <img src={(pathname === path || isSubMenuSelected) ? recycleWhite : recycle} 
+          alt="" className="shrink-0 w-6 h-6" /> : logo}
           <p className="font-medium">{name}</p>
         </div>
-        {!path ? (expandedMenu === name || isSubMenuSelected) ? <PiCaretUp className="my-auto" /> : <PiCaretDown className="my-auto" /> : <></> }
+        {!path ? expandedMenu === name ? <PiCaretUp className="my-auto" /> : <PiCaretDown className="my-auto" /> : <></> }
       </div>
  
-      <div className={`overflow-hidden transition-all duration-[350ms] ${((!path && expandedMenu === name) || isSubMenuSelected) ?
+      <div className={`overflow-hidden transition-all duration-[350ms] ${(!path && expandedMenu === name) ?
          `mt-2 ${subMenu.length > 2 ? 'h-[120px]' : 'h-20'}` : 'w-0 h-0'}`}>
         {subMenu?.map((item, index) => (
           <div onClick={() => navigate(item.path)} key={index} className={`pl-12 flex gap-4 py-2 rounded-lg 
