900f218a82e9a174cc73ef1278fc30779cbb50c9
Felicio Angga <felicioangga004@gmail.com>
2023-11-13 23:57:35 +0700

    add fixed sidebar

--- src/components/navigation/SideBar.jsx
+++ src/components/navigation/SideBar.jsx
@@ -3,13 +3,13 @@ import recyThingLogo from "@/assets/recything-logo.png";
 import miniLogo from "@/assets/logo.png";
 import { SideBarItem } from "./SideBarItem";
 import { sideBarMenuArray } from "./menuData";
+import PropTypes from 'prop-types';
 
-export function SideBar() {
-	const [collapse, setCollapse] = useState(false);
+export function SideBar({ collapse, setCollapse }) {
 	const [expandedMenu, setExpandedMenu] = useState("");
 
 	return (
-		<div className={`px-6 pt-8 transition-all ${collapse ? 'w-[96px] min-w-[96px]' : 'w-[312px] min-w-[312px]'}  min-h-screen border-r border-[#C7C9D9]`}>
+		<div className={`fixed px-6 pt-8 transition-all ${collapse ? 'w-[96px] min-w-[96px]' : 'w-[312px] min-w-[312px]'} bg-white z-10 min-h-screen border-r border-[#C7C9D9]`}>
 			<img onClick={() => setCollapse(prev => !prev)} className="mx-auto transition-all" src={collapse ? miniLogo : recyThingLogo} alt="recyThingLogo" />
 			<div className="my-8 h-[1px] w-full bg-[#3BA639]" />
 			<div className="flex flex-col gap-4">
@@ -18,4 +18,9 @@ export function SideBar() {
 			</div>
 		</div>
 	);
+}
+
+SideBar.propTypes = {
+	collapse: PropTypes.bool,
+	setCollapse: PropTypes.func,
 }
