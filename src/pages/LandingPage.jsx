f441c4e20c51e3053c1d5fdcbd84e5bcb07e7b1e
Leonardiwijaya <wijayaleonardi5@gmail.com>
2023-11-15 00:03:56 +0700

    merge with developer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,7 +6,7 @@ import Footer from "@/components/footer/Footer";
 const LandingPage = () => {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Jumbotron />
       <AboutListCard />
       <Frame />
