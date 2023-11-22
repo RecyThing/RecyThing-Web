3f95f57ac181a79aef3084fdba6a595a621eac79
I Made Sudarsana Taksa Wibawa <98959851+TaksaWibawa@users.noreply.github.com>
2023-11-22 20:40:09 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ManageWasteExchange from "./pages/ManageWasteExchange";
 import UserDetail from "./pages/UserDetail";
 import VoucherList from "./pages/VoucherList";
 import Download from "./pages/Download";
-import ErrorNotFound from "./error/ErrorNotFound";
+import ErrorNotFound from "./pages/ErrorNotFound";
 import DaftarTransaksi from "./pages/TransactionList";
 import DataDropPoint from "./pages/DataDropPoint";
 import DataCustomization from "./pages/DataCustomization";
