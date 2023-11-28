20a1008e696714f7fa7b3de73855adf08b5b0139
I Made Sudarsana Taksa Wibawa <98959851+TaksaWibawa@users.noreply.github.com>
2023-11-28 17:01:43 +0800

    bugfix(input-date): fix bug by converting date from utc to local date

--- src/utils/format-date/formatDateToISOString.js
+++ src/utils/format-date/formatDateToISOString.js
@@ -1,4 +1,6 @@
 export function formatDateToISOString(date) {
 	if (!date) return null;
-	return new Date(date).toISOString().slice(0, 10);
+	const localDate = new Date(date);
+	localDate.setMinutes(localDate.getMinutes() - localDate.getTimezoneOffset());
+	return localDate.toISOString().slice(0, 10);
 }
