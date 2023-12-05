f9d76ddcf3b53e665dddb3189932063dc776dbd8
I Made Sudarsana Taksa Wibawa <98959851+TaksaWibawa@users.noreply.github.com>
2023-12-06 05:38:20 +0800

    feat(401-handler): add condition when response is 401

--- src/configs/axios/index.js
+++ src/configs/axios/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 import axios from "axios";
 import { authService } from "../auth";
+import { globalRoute } from "@/utils";
 
 export const axiosInstance = axios.create({
 	baseURL: process.env.VITE_API_URL,
@@ -21,3 +22,16 @@ axiosInstance.interceptors.request.use((config) => {
 
 	return config;
 });
+
+axiosInstance.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	(error) => {
+		if (error.response.status === 401) {
+			authService.clearCredentialsFromCookie();
+			globalRoute.navigate && globalRoute.navigate("/login");
+		}
+		return Promise.reject(error);
+	}
+);
