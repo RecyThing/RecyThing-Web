61f45e50c67e627e2c18d35cffd5e6b92d121b68
IrfanM66 <civilwar776677@gmail.com>
2023-12-06 16:34:39 +0700

    update logo lock password

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,3 +2,5 @@ export { APIAuth } from "./APILogin";
 export { APIUser } from "./APIUser";
 export { APIVoucher } from "./APIVoucher";
 export { APIAdmin } from "./APIAdmin";
+export { APITrashes } from "./APITrashes";
+export { APIArticle } from "./APIArticle";
